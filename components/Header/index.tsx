28f6ec2d13222c667c3cdf4b184ae08b7dd8d36c
Laniran JohnPaul <jplaniran01@gmail.com>
2024-08-09 12:12:41 +0100

    header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,65 @@
-const Header = () => {
-  return <div>Header</div>;
+import React, { useEffect } from "react";
+import { Navlinks } from "../../data";
+import Button from "../Button";
+import headerStyles from "./Header.module.scss";
+
+const Header: React.FC = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 100) {
+        const header = document.querySelector(
+          `.${headerStyles.header}`
+        ) as HTMLElement;
+        if (header) {
+          header.style.borderBottom = "1px solid #dadadb";
+        }
+      } else {
+        const header = document.querySelector(
+          `.${headerStyles.header}`
+        ) as HTMLElement;
+        if (header) {
+          header.style.borderBottom = "none";
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={headerStyles.header}>
+      <div className={headerStyles.logo}>
+        <img
+          src="/icons/brinte-icon.svg"
+          alt="logo"
+          width={22.06}
+          height={18.88}
+        />
+        <img
+          src="/icons/brinte-logo.svg"
+          alt="logo"
+          width={61.29}
+          height={16.56}
+        />
+        <div className={headerStyles.status}>BETA</div>
+      </div>
+      <div className={headerStyles.navigationLinks}>
+        {Navlinks.map((link) => (
+          <a href={link.to} key={link.name} className={headerStyles.link}>
+            {link.name}{" "}
+          </a>
+        ))}
+      </div>
+      <div className={headerStyles.headerCTA}>
+        <Button variant="tertiary">Login </Button>
+        <Button variant="secondary">Join waitlist</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ data/index.ts
@@ -0,0 +1,26 @@
+export const Navlinks = [
+  {
+    name: "Products",
+    to: "#",
+  },
+  {
+    name: "Customers",
+    to: "#",
+  },
+  {
+    name: "Docs",
+    to: "#",
+  },
+  {
+    name: "Our story",
+    to: "#",
+  },
+  {
+    name: "Pricing",
+    to: "#",
+  },
+  {
+    name: "Contact",
+    to: "#",
+  },
+];
